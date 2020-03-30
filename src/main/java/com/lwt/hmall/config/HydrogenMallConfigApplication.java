6aefa81a23ab9857b8c11c4ae13f9434d9cc1bb4
liwentao0a <1642969272@qq.com>
2020-03-30 11:41:09 +0800

    Initial commit

---
+++ src/main/java/com/lwt/hmall/config/HydrogenMallConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lwt.hmall.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrogenMallConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrogenMallConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lwt/hmall/config/HydrogenMallConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwt.hmall.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrogenMallConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
